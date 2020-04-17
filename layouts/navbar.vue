91651eca3277bebcdb49c8d66a880c9f6e9cfcdb
Nathan Fishel <nfishel@Nathans-MacBook-Air.local>
2020-04-17 16:33:01 -0400

    added photo to index

---
+++ layouts/navbar.vue
@@ -0,0 +1,51 @@
+
+
+<template>
+  <div id ="content">
+    <div id="main-nav">
+      <b-navbar>
+          <template slot="brand">
+              <b-navbar-item tag="router-link" :to="{ path: '/' }">
+              <h3>Nathaniel Fishel</h3>
+                  <!--
+                  <img
+                      src="https://raw.githubusercontent.com/buefy/buefy/dev/static/img/buefy-logo.png"
+                      alt="Lightweight UI components for Vue.js based on Bulma"
+                  >
+                  -->
+              </b-navbar-item>
+          </template>
+          <template slot="start">
+              <b-navbar-item tag="a" href="#">
+                  Home
+              </b-navbar-item>
+              <b-navbar-item href="#">
+                  Portflolio
+              </b-navbar-item>
+              <b-navbar-dropdown label="Info">
+                  <b-navbar-item href="#">
+                      About
+                  </b-navbar-item>
+                  <b-navbar-item href="#">
+                      Resume
+                  </b-navbar-item>
+                  <b-navbar-item href="#">
+                      Contact
+                  </b-navbar-item>
+              </b-navbar-dropdown>
+          </template>
+
+          <template slot="end">
+              <b-navbar-item tag="div">
+                  <div class="buttons">
+                      <a class="button is-primary">
+                          <strong>Contact Me</strong>
+                      </a>
+                  </div>
+              </b-navbar-item>
+          </template>
+      </b-navbar>
+  </div>
+  <nuxt/>
+</div>   
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,6 +1,75 @@
 <template>
-  <section class="section">
+  <section class="section" style="padding:0px;">
+    
+    <!-- Front page photo-->
+      <section class="hero bg-img is-success is-fullheight">
+    <!-- Hero head: will stick at the top -->
+    <div class="hero-head">
+      <header class="navbar">
+        <div class="container">
+          <div class="navbar-brand">
+          <h1>Nathaniel Fishel</h1>
+          <!--
+            <a class="navbar-item">
+              <img src="https://bulma.io/images/bulma-type-white.png" alt="Logo">
+            </a>
+            -->
+            <span class="navbar-burger burger" data-target="navbarMenuHeroC">
+              <span></span>
+              <span></span>
+              <span></span>
+            </span>
+          </div>
+          <div id="navbarMenuHeroC" class="navbar-menu">
+            <div class="navbar-end">
+              <a class="navbar-item is-active">
+                Home
+              </a>
+              <a class="navbar-item">
+                About me
+              </a>
+              <a class="navbar-item">
+                Portfolio
+              </a>
+              <span class="navbar-item">
+                <a class="button is-success is-inverted">
+                  <span class="icon">
+                    <i class="fab fa-github"></i>
+                  </span>
+                  <span>Contact me</span>
+                </a>
+              </span>
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
+
+    <!-- Hero content: will be in the middle -->
+    <div class="hero-body">
+      <div class="container has-text-centered">
+        <h1 id="text" class="title" onload="typewriter()">
+          
+        </h1>
+        <h2 class="subtitle">
+          Subtitle
+        </h2>
+      </div>
+    </div>
+
+    <!-- Hero footer: will stick at the bottom -->
+    <div class="hero-foot">
+      <nav class="tabs is-boxed is-fullwidth">
+        <div class="container">
+          <!-- bottom tabs-->
+        </div>
+      </nav>
+    </div>
+
+  </section>
+  
     <div class="columns is-mobile">
+    
       <card
         title="Free"
         icon="github-circle"
@@ -45,9 +114,23 @@ import Card from '~/components/Card'
 
 export default {
   name: 'HomePage',
+  
 
   components: {
     Card
   }
 }
+
+var i = 0;
+var txt = 'Lorem ipsum dummy text blabla.';
+var speed = 50;
+
+function typeWriter() {
+  if (i < txt.length) {
+    document.getElementById("text").innerHTML += txt.charAt(i);
+    i++;
+    setTimeout(typeWriter, speed);
+  }
+}
+
 </script>
