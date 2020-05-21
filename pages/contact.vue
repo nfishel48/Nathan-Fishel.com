2e1b8e9f74829a0ade40df80fe1c709faea44539
Nathan Fishel <nfishel@Nathans-MacBook-Air.local>
2020-05-21 17:21:22 -0400

    rebuilt contact form

--- pages/contact.vue
+++ pages/contact.vue
@@ -2,43 +2,55 @@
     <section>
         <h1 class="main-text" style=" padding-bottom: 1%;">Contact me</h1>
         <h2 class="main-body" style="font-size:150%; padding-left: 5%;">Please fill out the form below to reach me</h2>
-        <form id="contact-form" 
-          name="contact"
-          method="post"
-          data-netlify="true"
-          data-netlify-honeypot="bot-field"
+  
         
+        <form
+            name="ask-question"
+            method="post"
+            data-netlify="true"
+            data-netlify-honeypot="bot-field"
         >
-            <b-field label="Name">
-                <b-input v-model="name"
-                type="text"
-                required
-              ></b-input>
-            </b-field>
-            
-            <b-field  label="Email"
-                >
-                <b-input 
-                    type="email"
-                    required
-                    maxlength="30">
-                </b-input>
-            </b-field>
-            
-            <b-field  label="Message">
-                <b-input maxlength="200" type="textarea"
-                required
-                ></b-input>
-            </b-field>
-
-            <b-button type="is-success" outlined>Submit</b-button>
+                    <input type="hidden" name="form-name" value="ask-question" />
+                    <p>
+                         <b-field label="Name" class="main-body" style="font-size:150%; padding-left: 5%;">
+                            <b-input v-model="name"></b-input>
+                        </b-field>
+                    </p>
+                    <p>
+                        <b-field label="Email" class="main-body" style="font-size:150%; padding-left: 5%;">
+                            <b-input v-model="email" type="email"></b-input>
+                        </b-field>
+                    </p>
+                    <p>
+                        <b-field label="Message" class="main-body" style="font-size:150%; padding-left: 5%;">
+                            <b-input maxlength="200" type="textarea" v-model="message"></b-input>
+                        </b-field>
+                    </p>
+        <div style="font-size:150%; padding-left: 5%;">
+            <b-button  type="is-success" outlined>Submit</b-button>
+        </div>
         </form>
+        
+
+
     </section>
 </template>
 
 <script>
+
 export default {
-    layout: 'navbar'
+    layout: 'navbar',
+    components: {
+    
+  },
+  data () {
+    return {
+      name: null,
+      email: null,
+      message: null
+    }
+  }
     
 }
+
 </script>
