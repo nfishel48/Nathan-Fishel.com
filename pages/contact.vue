f9258e5b007575d5e95df4f91cbd25b7fc09e07f
Nathan Fishel <nfishel@Nathans-MacBook-Air.local>
2020-05-24 14:00:12 -0400

    Form 0.0.9

--- pages/contact.vue
+++ pages/contact.vue
@@ -3,7 +3,8 @@
         <h1 class="main-text" style=" padding-bottom: 1%;">Contact me</h1>
         <h2 class="main-body" style="font-size:150%; padding-left: 5%;">Please fill out the form below to reach me</h2>
   
-    <form name="contact" netlify netlify-honeypot="bot-field" hidden>
+    <form name="contact" action="" method="post" netlify>
+        <input type="hidden" name="form-name" value="contact" />
         <label for="name">Name</label>
         <input type="text" name="name" />
         <label for="email">Email</label>
